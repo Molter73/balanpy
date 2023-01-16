b87916765bc06ab06b87097fe57cbffdf714c555
Alex Delgado <bmsjale@gmail.com>
2023-01-16 21:02:12 +0100

    In Progress - Pantalla Inicio
    Se ha mejorado la visualización de la pantalla, se ha añadido una imagen extra para el borde superior de la pantalla y se ha solucionado el problema con los botones, ahora hacen una simple impresión.

--- src/main/java/org/balanpy/Inicio.java
+++ src/main/java/org/balanpy/Inicio.java
@@ -1,20 +1,15 @@
 package org.balanpy;
 
-import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
 
 public class Inicio {
 
-	public void HandleDone(ActionEvent event) throws Exception {
+	public void HandleDone(MouseEvent event) throws Exception {
 		
 		System.out.println("Está funcionando crack");
 		
 	}
 	
-	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
-			
-
-	}
 }
