ad00b4a02b5436815ec7f735c42106c633859c71
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-01-16 10:49:10 +0100

    Add weather screen (#12)

---
+++ src/main/java/org/balanpy/ClimaController.java
@@ -0,0 +1,56 @@
+package org.balanpy;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.util.Pair;
+
+public class ClimaController {
+	@FXML
+	Label avisoPaseo;
+
+	@FXML
+	Label temperaturaActual;
+
+	@FXML
+	LineChart<String, Double> tempChart;
+
+	static final private Color WARNING = Color.RED;
+	static final private Color OK = Color.GREEN;
+
+	@FXML
+	public void initialize() throws UnsupportedOperationException, IOException {
+		ClimaClient clima = new ClimaClient();
+		Double temp = clima.getTemperaturaActual();
+
+		temperaturaActual.setText(temp + " ºC");
+		setAvisoPaseo(temp);
+		fillTempGraph(clima.getTemperaturasPorHora());
+	}
+
+	private void setAvisoPaseo(Double temp) {
+		if (temp < 15 || temp > 30) {
+			avisoPaseo.setText("Condiciones adversas, permanezca en interiores");
+			avisoPaseo.setTextFill(WARNING);
+		} else {
+			avisoPaseo.setText("Condiciones ideales para pasear");
+			avisoPaseo.setTextFill(OK);
+		}
+	}
+
+	private void fillTempGraph(ArrayList<Pair<String, Double>> temperaturas) {
+		XYChart.Series<String, Double> series = new XYChart.Series<>();
+
+		for (Pair<String, Double> temp : temperaturas) {
+			series.getData().add(new XYChart.Data<String, Double>(temp.getKey(), temp.getValue()));
+		}
+
+		series.setName("Temperatura en ºC");
+		tempChart.getData().add(series);
+	}
+}
