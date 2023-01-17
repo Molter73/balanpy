1e57b5c82416159f8550e6575abe888120166fe0
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2023-01-17 18:39:11 +0100

    Pantalla Inicio Balanpy (#13)
    * Creación de Pantalla de Inicio
    
    Aun no hay cambios
    
    * Creación Pantalla Inicio
    
    Creación de la pantalla a través de archivo fxml PantallaInicio y su clase correspondiente Inicio
    
    * Pantalla Inicio Arreglado
    
    He adjuntado a recursos las imagenes disponibles, solucionado el css y los botones de la aplicación.
    
    * In Progress - Pantalla Inicio
    
    Se ha mejorado la visualización de la pantalla, se ha añadido una imagen extra para el borde superior de la pantalla y se ha solucionado el problema con los botones, ahora hacen una simple impresión.
    
    * Limpieza Clase Inicio.java
    
    Se han borrado los imports que sobraban de la clase Inicio.java

---
+++ src/main/java/org/balanpy/Inicio.java
@@ -0,0 +1,13 @@
+package org.balanpy;
+
+import javafx.scene.input.MouseEvent;
+
+public class Inicio {
+
+	public void HandleDone(MouseEvent event) throws Exception {
+		
+		System.out.println("Está funcionando crack");
+		
+	}
+	
+}
--- src/main/java/org/balanpy/Main.java
+++ src/main/java/org/balanpy/Main.java
@@ -30,7 +30,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		LoadScene(primaryStage, "/Clima.fxml");
+		LoadScene(primaryStage, "/PantallaInicio.fxml");
 	}
 
 	//-----------------------------------------------------------------------------------------
