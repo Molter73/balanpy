27e8485bd4aacab3794f0e6ef13ebd850ca35ea4
Alex Delgado <bmsjale@gmail.com>
2023-01-18 18:19:07 +0100

    In Progress - Perfil de Usuario
    Se ha realizado la pantalla de Perfil de Usuario, con su correspondiente clase java PerfilUsuario que servirá de controlador de la pantalla. 
    
    No se ha cubierto la parte inferior de los botones para tener la referencia de la Portada de la Aplicación.
    
    Hay que añadir el CSS a los botones para que tengan un diseño unificado.

---
+++ src/main/java/org/balanpy/PerfilUsuario.java
@@ -0,0 +1,13 @@
+package org.balanpy;
+
+import javafx.scene.input.MouseEvent;
+
+public class PerfilUsuario {
+
+public void HandleDone(MouseEvent event) throws Exception {
+		
+		System.out.println("Está funcionando crack");
+		
+	}
+}
+
