629e51b93656eb211dd976f36439fa38e260b2c2
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-01-20 19:27:44 +0100

    Add Usuario interface (#16)

---
+++ src/main/java/org/balanpy/Usuario.java
@@ -0,0 +1,61 @@
+package org.balanpy;
+
+/*
+ * La información del usuario se guardará en un fichero
+ * JSON con la siguiente información:
+
+usuario: {
+  "DNI": "29863077A",
+  "nombre": "Jorge Fulanito",
+  "telefono": "+3412345678",
+  "email": "jfulanito@hotmail.com",
+  "sexo": "masculino",
+  "direccion": "Calle False 123",
+  "archivo_ppp": "29863077A/ppp.txt",
+  "archivo_seguro": "29863077A/seguro.txt"
+}
+
+ */
+public interface Usuario {
+	// getters
+	public String getDNI();
+
+	public String getNombre();
+
+	public String getTelefono();
+
+	public String getEmail();
+
+	public String getSexo();
+
+	public String getDireccion();
+
+	public String getPathPPP();
+
+	public String getPathSeguro();
+
+	// setters
+	public void setDNI(String dni);
+
+	public void setNombre(String nombre);
+
+	public void setTelefono(String telefono);
+
+	public void setEmail(String email);
+
+	public void setSexo(String sexo);
+
+	public void setDireccion(String direccion);
+
+	public void setPathPPP(String pathPPP);
+
+	public void setPathSeguro(String seguroPath);
+
+	// Special functionality
+
+	// Helper function for persisting user information
+	public void save();
+
+	// Helper for reloading the object
+	public void reload();
+}
