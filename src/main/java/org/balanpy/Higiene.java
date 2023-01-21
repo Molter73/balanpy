68dcb7b4c1002de01c7ba1677e40d5c03af60760
Alex Delgado <bmsjale@gmail.com>
2023-01-21 13:10:31 +0100

    Interfaz Mascota - Cambios Realizados
    Se ha reducido los espacios dentro del código y aplicado los cambios comentados en el commit.

--- src/main/java/org/balanpy/Higiene.java
+++ src/main/java/org/balanpy/Higiene.java
@@ -4,72 +4,29 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
 
-/*
- * * La información de la Mascota se guardará en un fichero
- * JSON con la siguiente información:
- * 
-    "higiene": {
-      "ultimo_bano": "02/02/2022",
-      "ultimo_cepillado": "02/02/2022",
-      "ultimo_desparasitado": {
-        "interno": "02/02/2022",
-        "externo": "02/02/2022"
-      },
-      "productos_no_recomendables": []
-    }
-  }
-]
- */
-
-
 public interface Higiene {
 
-	//************************ Baño **************************
-	
-	public ArrayList<Date> getBanos();		//GETTER
-	
-	public void setBanos(ArrayList<Date> banos);		//SETTER
-	
-	
-	//********************* Cepillado ************************
-	
-	public ArrayList<Date> getCepillado();	//GETTER
-		
-	public void setCepillado(ArrayList<Date> cepillado);	//SETTER
-	
-	
-	//******************* Desparasitación ********************
+	// ************************ Baño **************************
 
-	public Map<String, Date> getDesparasitado();	//GETTER
+	public ArrayList<Date> getBanos(); // GETTER
 
-	public void setDesparasitado(Map<String, Date> desparasitado);	//SETTER
-	
-	
-	//************* Productos No Recomendables ***************
-	
-	public ArrayList<String> getProductosNR();	//GETTER
-			
-	public void setProductosNR(ArrayList<String> productosNR);	//SETTER
-	
-	
-	// Special functionality
+	public void setBanos(ArrayList<Date> banos); // SETTER
 
-	// Helper function for persisting user information
-	public void save();
+	// ********************* Cepillado ************************
 
-	// Helper for reloading the object
-	public void reload();
-	
-	
+	public ArrayList<Date> getCepillado(); // GETTER
+
+	public void setCepillado(ArrayList<Date> cepillado); // SETTER
+
+	// ******************* Desparasitación ********************
+
+	public Map<String, Date> getDesparasitado(); // GETTER
+
+	public void setDesparasitado(Map<String, Date> desparasitado); // SETTER
+
+	// ************* Productos No Recomendables ***************
+
+	public ArrayList<String> getProductosNR(); // GETTER
+
+	public void setProductosNR(ArrayList<String> productosNR); // SETTER
 }
-
-
-
-
-
-
-
-
-
-
-
--- src/main/java/org/balanpy/Mascota.java
+++ src/main/java/org/balanpy/Mascota.java
@@ -3,100 +3,57 @@ package org.balanpy;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
-
-/*
- * * La información de la Mascota se guardará en un fichero
- * JSON con la siguiente información:
- * 
- * mascotas:
-[
-  {
-    "UUID": "123456",
-    "fecha_nacimiento": "02/02/2022",
-    "numero_chip": "ABCDE12345",
-    "raza": "pitbull",
-    "sexo": "masculino",
-    "peso": 25,
-    "color": "blanco",
-    "grupo_sanguineo": "A",
-    "nombre": "Firulais",
-    "archivo_pedigree": "123456/pedigree.txt",
-    "archivo_dni": "123456/dni.txt",
-    "archivo_esterilizacion": "123456/esterilizacion.txt",
-    "temperaturas": "123456/temperaturas.json",
-    "alergias": [
-      "polen",
-      "cereales"
-    ],
-    "vacunas": {
-      "ultima": "02/02/2022",
-      "proxima": "02/02/2023"
-    },
-    "enfermedades": [],
-    "higiene": {
-      "ultimo_bano": "02/02/2022",
-      "ultimo_cepillado": "02/02/2022",
-      "ultimo_desparasitado": {
-        "interno": "02/02/2022",
-        "externo": "02/02/2022"
-      },
-      "productos_no_recomendables": []
-    }
-  }
-]
- */
-
+import java.util.UUID;
 
 public interface Mascota {
-	
-	//GETTERS
-	
-	public int getUUID();
-	
+
+	// GETTERS
+
+	public UUID getUUID();
+
 	public Date getFechaNacimiento();
-	
+
 	public String getChip();
-	
+
 	public String getRaza();
-	
-	public String getSexoMascota();
-	
+
+	public String getSexo();
+
 	public int getPeso();
-	
+
 	public String getColor();
-	
+
 	public String getGrupoSanguineo();
-	
-	public String getNombreMascota();
-	
+
+	public String getNombre();
+
 	public String getPathPedigree();
-	
+
 	public String getPathDNI();
-	
+
 	public String getPathEsterilización();
-	
+
 	public String getPathTemperaturas();
 
+	// SETTERS
 
-	//SETTERS
-	
-	public void setUUID(int uuid);
+	public void setUUID(UUID uuid);
 
-	public void setFechaNacimiento(Date fecha_nacimiento);
-	
+	public void setFechaNacimiento(Date fechaNacimiento);
+
 	public void setChip(String chip);
 
 	public void setRaza(String raza);
 
-	public void setSexoMascota(String sexo_mascota);
-	
+	public void setSexo(String sexo);
+
 	public void setPeso(int peso);
-	
+
 	public void setColor(String color);
 
-	public void setGrupoSanguineo(String grp_sanguineo);
+	public void setGrupoSanguineo(String grupoSanguineo);
 
-	public void setNombreMascota(String nom_mascota);
+	public void setNombre(String nombre);
 
 	public void setPathPedigree(String pathPedigree);
 
@@ -106,40 +63,21 @@ public interface Mascota {
 
 	public void setPathTemperaturas(String pathTemperaturas);
 
-	
-	//********************** Alergias *****************************
-	
-	public ArrayList<String> getAlergias();		//GETTER
-	
-	public void setAlergias(ArrayList<String> alergias);		//SETTER
-	
-	
-	
-	
-	//******************** Enfermedades ***************************
+	// ********************** Alergias *****************************
 
-	public ArrayList<String> getEnfermedades();		//GETTER
+	public ArrayList<String> getAlergias(); // GETTER
 
-	public void setEnfermedades(ArrayList<String> enfermedades);		//SETTER
-	
-	
-	
-	//********************** Vacunas ******************************
-	
-	public Map<String, Date> getVacuna();		//GETTER
+	public void setAlergias(ArrayList<String> alergias); // SETTER
 
-	public void setVacunas(Map<String, Date> vacunas);		//SETTER
+	// ******************** Enfermedades ***************************
 
-	
-	
-	// Special functionality
+	public ArrayList<String> getEnfermedades(); // GETTER
 
-	// Helper function for persisting user information
-	public void save();
+	public void setEnfermedades(ArrayList<String> enfermedades); // SETTER
 
-	// Helper for reloading the object
-	public void reload();
-	
-	
+	// ********************** Vacunas ******************************
 
+	public Map<String, Date> getVacuna(); // GETTER
+
+	public void setVacunas(Map<String, Date> vacunas); // SETTER
 }
