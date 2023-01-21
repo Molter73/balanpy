6bfee5d1d8752fbed3000785e38369821948814b
Alzaro207 <117013444+Alzaro207@users.noreply.github.com>
2023-01-21 15:42:27 +0000

    Add PerfilMascota.fxml y .java

---
+++ src/main/java/org/balanpy/PerfilMascota.java
@@ -0,0 +1,25 @@
+package org.balanpy;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+
+public class PerfilMascota {
+	@FXML
+CheckBox configuracion_mascota;
+	
+CheckBox salud_mascota;
+
+CheckBox seguro_mascota;
+
+CheckBox dni_mascota;
+
+CheckBox pedigree;
+
+CheckBox chip;
+
+CheckBox esterilizado;
+
+CheckBox ppp;
+	
+}
