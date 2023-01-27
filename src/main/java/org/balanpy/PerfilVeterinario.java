a4f71a72482965d96802e2be35f1bf977c1e6d75
Nataliafm <n.fajardom1@gmail.com>
2023-01-27 16:21:37 +0100

    cambios

--- src/main/java/org/balanpy/PerfilVeterinario.java
+++ src/main/java/org/balanpy/PerfilVeterinario.java
@@ -2,38 +2,11 @@ package org.balanpy;
 
 import javafx.scene.input.MouseEvent;
 
-public class PerfilVeterinar {
-
-import javafx.scene.input.MouseEvent;
+public class PerfilVeterinario {
 
 public void HandleDone(MouseEvent event) throws Exception {
 		
 		System.out.println("Está funcionando crack");
 		
 	}
-/*
-//************* Controller.java *************
-package application;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-
-public class Controller {
-
-@FXML
-private DatePicker myDatePicker;
-@FXML
-private Label myLabel;
-
-public void getDate(ActionEvent event) {
-
-LocalDate myDate = myDatePicker.getValue();
-String myFormattedDate = myDate.format(DateTimeFormatter.ofPattern("MMM-dd-yyyy"));
-myLabel.setText(myFormattedDate);
 }
-}
-*/
