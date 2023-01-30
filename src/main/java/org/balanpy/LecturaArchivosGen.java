60e0eb2e52ff279f355753d6b8fef5ac1ff3e9e7
Alex Delgado <bmsjale@gmail.com>
2023-01-30 13:57:02 +0100

    Creación Clase Lectura Ficheros
    Se ha creado la clase de lectura de ficheros.

--- src/main/java/org/balanpy/LecturaArchivosGen.java
+++ src/main/java/org/balanpy/LecturaArchivosGen.java
@@ -1,5 +1,47 @@
 package org.balanpy;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LecturaArchivosGen {
 
+	public static void main(String[] args) {
+
+		Leer_Fichero accediendo = new Leer_Fichero();
+
+		accediendo.leeFichero();
+
+	}
+
+}
+
+class Leer_Fichero {
+
+	public void leeFichero() {
+
+		try {
+
+			FileReader entrada = new FileReader(" ");
+
+			int c = entrada.read();
+
+			while (c != -1) {
+
+				c = entrada.read();
+
+				char letra = (char) c;
+
+				System.out.print(letra);
+
+			}
+
+			entrada.close();
+
+		} catch (IOException e) {
+
+			// TODO Bloque catch generado automáticamente
+
+			System.out.println("No se ha encontrado el archivo.");
+		}
+	}
 }
