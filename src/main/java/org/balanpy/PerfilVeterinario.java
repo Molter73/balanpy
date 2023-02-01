636070bd28bfc5ff52752ce7197fe350b5aa1fd2
Natalia Fajardo Marzolla <93014301+Nataliafm1@users.noreply.github.com>
2023-02-01 19:30:16 +0100

    Perfilveterinario (#23)
    * nuevos
    
    * perfil veterinario creado
    
    * cambios
    
    * ubicacion de la imagen corregida
    
    * cambios (quitar vacunas.java y agregar })
    
    * cambios
    
    * agregadas imagenes y rediseno de la pantalla
    
    * quitado el pie de página, soy incapaz de poner el que ya tenemos de barrada de tareas :(
    
    * agregada la barra de tareas pero necesito poder bajarla

---
+++ src/main/java/org/balanpy/PerfilVeterinario.java
@@ -0,0 +1,12 @@
+package org.balanpy;
+
+import javafx.scene.input.MouseEvent;
+
+public class PerfilVeterinario {
+
+	public void HandleDone(MouseEvent event) throws Exception {
+		
+		System.out.println("Está funcionando crack");
+		
+	}
+}
