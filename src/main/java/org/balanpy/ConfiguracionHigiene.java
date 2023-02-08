7d539f4bdf031ab7d9c0658ad7d6fdf6e1e240e3
Alzaro207 <alzaro_206@hotmail.com>
2023-02-08 01:10:47 +0000

    Modificacion de la pantalla ConfiguracionMascota.fxlm y creacion del .java

---
+++ src/main/java/org/balanpy/ConfiguracionHigiene.java
@@ -0,0 +1,12 @@
+package org.balanpy;
+
+import javafx.scene.input.MouseEvent;
+
+public class ConfiguracionHigiene {
+
+	public void HandleDone(MouseEvent event) throws Exception {
+
+		System.out.println("Está funcionando amigo");
+
+	}
+}
