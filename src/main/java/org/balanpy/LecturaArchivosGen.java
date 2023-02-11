8f796fa778f00461af098e89d636ed423ce8a3b4
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2023-02-11 18:59:35 +0100

    Clase Lectura Archivos Genéricos
    Se ha creado la clase de Lectura de Archivos Genéricos

---
+++ src/main/java/org/balanpy/LecturaArchivosGen.java
@@ -0,0 +1,55 @@
+package org.balanpy;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class LecturaArchivosGen {
+
+	@FXML
+	private Label fileNameLabel;
+
+	@FXML
+	private TextArea fileContentTextArea;
+
+	public void openFile(File file) throws IOException {
+
+		// Obtiene el contenido del archivo
+		String content = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())), StandardCharsets.UTF_8);
+
+		// Carga la vista de archivo
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("FileView.fxml"));
+
+		loader.setController(this);
+
+		AnchorPane root = loader.load();
+
+		// Configura la escena y el stage
+		Scene scene = new Scene(root);
+		Stage stage = new Stage();
+
+		stage.setScene(scene);
+
+		stage.setTitle("Archivo: " + file.getName());
+
+		stage.initModality(Modality.APPLICATION_MODAL);
+
+		// Muestra el contenido del archivo
+		fileNameLabel.setText(file.getName());
+
+		fileContentTextArea.setText(content);
+
+		stage.showAndWait();
+	}
+}
