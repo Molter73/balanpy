e173f5ee448cc6ccd49c0d69128ae36feab63c25
Alex Delgado <bmsjale@gmail.com>
2023-02-11 02:44:13 +0100

    Creadas las Transiciones desde Portada Aplicación
    Se han creado las transiciones desde la Portada hasta cada uno de los botones que aparecen en la misma. 
    Además, se le ha añadido un fx:id a los botones dentro de la portada de la aplicación.

--- src/main/java/org/balanpy/PortadaAplicacion.java
+++ src/main/java/org/balanpy/PortadaAplicacion.java
@@ -1,12 +1,80 @@
 package org.balanpy;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class PortadaAplicacion {
+public class PortadaAplicacion implements Initializable {
 
-	public void HandleDone(MouseEvent event) throws Exception {
-		
-		System.out.println("Está funcionando crack");
-		
+	private Stage stage;
+
+	private static final String SCREEN = "/PortadaAplicacion.fxml";
+
+	public void setStage(Stage stage) {
+
+		this.stage = stage;
+
 	}
+
+	BalanpyScreen balanpyScreen = new BalanpyScreen();
+
+	@FXML
+	VBox temperaturaExterna;
+
+	@FXML
+	VBox higiene;
+
+	@FXML
+	VBox vacunas;
+
+	@FXML
+	VBox registroMascota;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+	public void temperaturaExterna(MouseEvent event) throws IOException {
+
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		BalanpyScreen.loadScene(stage, "/temperaturaExterna.fxml", SCREEN);
+	}
+
+	public void higiene(MouseEvent event) throws IOException {
+
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		BalanpyScreen.loadScene(stage, "/higiene.fxml", SCREEN);
+	}
+
+	public void vacunas(MouseEvent event) throws IOException {
+
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		BalanpyScreen.loadScene(stage, "/vacunas.fxml", SCREEN);
+	}
+
+	public void registroMascota(MouseEvent event) throws IOException {
+
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		BalanpyScreen.loadScene(stage, "/ConfiguracionMascota.fxml", SCREEN);
+	}
+
+	public void perfilMascota(MouseEvent event) throws IOException {
+
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		BalanpyScreen.loadScene(stage, "/PerfilMascota.fxml", SCREEN);
+	}
+
 }
