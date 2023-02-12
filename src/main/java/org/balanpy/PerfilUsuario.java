8c6f479edf16565373b07294779d88c922884f6e
Alzaro207 <alzaro_206@hotmail.com>
2023-02-12 19:46:17 +0000

    Accesos desde PerfilUsuario

--- src/main/java/org/balanpy/PerfilUsuario.java
+++ src/main/java/org/balanpy/PerfilUsuario.java
@@ -1,11 +1,17 @@
 package org.balanpy;
 
-import java.awt.Label;
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class PerfilUsuario {
+	private static final String SCREEN = "/PerfilUsuario.fxml";
+
 	@FXML
 	private Label nombre;
 
@@ -23,6 +29,24 @@ public class PerfilUsuario {
 
 	@FXML
 	private Label direccion;
+
+	public void inicialize() {
+
+		Usuario usuario = UsuarioImpl.getInstance();
+
+		nombre.setText(usuario.getNombre());
+		edad.setText(String.valueOf(usuario.getEdad()));
+		telefono.setText(usuario.getTelefono());
+		email.setText(usuario.getEmail());
+		sexo.setText(usuario.getSexo());
+		direccion.setText(usuario.getDireccion());
+
+	}
+
+	public void editarPerfil(ActionEvent event) throws IOException {
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		BalanpyScreen.loadScene(stage, "/EditarPerfilUsuario.fxml", SCREEN);
+	}
 
 	public void HandleDone(MouseEvent event) throws Exception {
 
