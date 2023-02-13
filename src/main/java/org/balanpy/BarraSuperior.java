47f8be9f6a0eff1daef5da9d83ca91d5c7d271da
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2023-02-13 21:00:57 +0100

    Barra Superior - In Progress (#53)
    Se ha creado la Barra Superior que se utilizará en todas las pantallas. Además, se ha creado la clase controladora de la misma.

---
+++ src/main/java/org/balanpy/BarraSuperior.java
@@ -0,0 +1,12 @@
+package org.balanpy;
+
+import javafx.scene.input.MouseEvent;
+
+public class BarraSuperior {
+
+	public void HandleDone(MouseEvent event) throws Exception {
+
+		System.out.println("Está funcionando amigo");
+
+	}
+}
