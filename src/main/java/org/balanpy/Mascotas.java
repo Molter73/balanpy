4424a7dad5715244b6b38e49b2d4c7e2a90b04fc
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-02-17 19:19:26 +0100

    Minor improvement

--- src/main/java/org/balanpy/Mascotas.java
+++ src/main/java/org/balanpy/Mascotas.java
@@ -2,7 +2,6 @@ package org.balanpy;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
@@ -13,7 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Mascotas {
 	private static ArrayList<MascotaImpl> mascotas;
-	private static final Path MASCOTAS_PATH = Paths.get(Configuracion.getRootDir(), "mascotas.json");
 
 	private Mascotas() {
 	}
@@ -33,7 +31,8 @@ public class Mascotas {
 	public static ArrayList<MascotaImpl> load() {
 		try {
 			ObjectMapper om = new ObjectMapper();
-			mascotas = om.readValue(MASCOTAS_PATH.toFile(), new TypeReference<ArrayList<MascotaImpl>>() {
+			mascotas = om.readValue(Configuracion.getMascotasPath().toFile(),
+					new TypeReference<ArrayList<MascotaImpl>>() {
 			});
 		} catch (IOException e) {
 			System.out.println("Failed to load pet data. " + e.getMessage());
